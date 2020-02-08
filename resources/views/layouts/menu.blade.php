149e823925b7431f3077773adf5d732fa3fd3743
Thomas van Gaalen <thomasvangaalen10@gmail.com>
2020-02-08 22:44:03 +0100

    Login and register without npm+ default layout with bootstrap

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,23 @@
+<ul class="nav justify-content-center">
+    <li class="nav-item">
+        <a class="nav-link" href="#">Active</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+    </li>
+
+    @auth
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+        </li>
+
+        {{-- logout must be POST with csrf --}}
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
+    @else
+        <a href="login">login</a>
+        <a href="register">register</a>
+    @endauth
+</ul>
