6f25a6ae6975e33a7fe840fd9f4367c56227a615
Thomas <thomasvangaalen10@gmail.com>
2020-04-23 21:21:31 +0200

    Route fixes

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::get('/dashboard', 'DashboardController@index')->name('home');
+Route::get('/home', 'DashboardController@index')->name('home');
 
 Route::resource('/docenten', 'TeacherController')->middleware('checkRole:admin');
 Route::resource('/vakken', 'CourseController')->middleware('checkRole:admin');
@@ -27,11 +24,5 @@ Route::post('/vakken/test/{course}', 'TestController@store')->middleware('checkR
 
 Route::post('/upload/assesment/{course}', 'TestController@uploadAssesment')->middleware('checkRole:admin');
 Route::post('/grade/{course}', 'testController@gradeAssesment')->middleware('checkRole:admin');
-
-
-//admin needs also auth middleware because if user isn't set auth()->user() is null
-Route::get('/admin', function () {
-    return view('admin');
-})->middleware('admin', 'auth');
 
 Route::get('/docenten', 'TeacherController@index');
